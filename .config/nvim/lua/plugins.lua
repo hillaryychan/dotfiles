5c6b40a5db7f4a7cb93e6185dbcc83155d132552
Hillary Chan <hillary.chan91@gmail.com>
2021-12-13 23:25:54 +1100

    Setup nvim-lspconfig and nvim-cmp

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -13,6 +13,12 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 Plug 'sainnhe/sonokai'                          -- colourscheme
 
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = vim.fn['TSUpdate']})
+Plug 'hrsh7th/nvim-cmp'                         -- autocompletion
+Plug 'hrsh7th/cmp-nvim-lsp'                     -- LSP source for nvim-cmp
+Plug 'hrsh7th/cmp-buffer'                       -- buffer source for nvim-cmp
+Plug 'hrsh7th/cmp-path'                         -- filsystem source for nvim-cmp
+Plug 'hrsh7th/cmp-cmdline'                      -- vim cmdline source for nvim-cmp
+Plug 'neovim/nvim-lspconfig'                    -- intellisense
 
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'            -- fuzzy finder
@@ -57,6 +63,137 @@ require('nvim-treesitter.configs').setup {
 vim.api.nvim_set_option('foldmethod', 'expr')
 vim.api.nvim_set_option('foldexpr', 'nvim_treesitter#foldexpr()')
 vim.api.nvim_set_option('foldlevelstart', 99)
+
+-- nvim-cmp
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<C-Space>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if not cmp.select_next_item() then
+        if vim.bo.buftype ~= 'prompt' and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if not cmp.select_prev_item() then
+        if vim.bo.buftype ~= 'prompt' and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end
+    end,
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'path' },
+  }),
+  snippet = {
+    -- Don't want to use actual snippet engine.
+    -- This snippet expander just inputs selected text and places cursor at the end.
+    expand = function(args)
+      local line_num, col = unpack(vim.api.nvim_win_get_cursor(0))
+      local line_text = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, true)[1]
+      local indent = string.match(line_text, '^%s*')
+      local replace = vim.split(args.body, '\n', true)
+      local surround = string.match(line_text, '%S.*') or ''
+      local surround_end = surround:sub(col)
+
+      replace[1] = surround:sub(0, col - 1)..replace[1]
+      replace[#replace] = replace[#replace]..(#surround_end > 1 and ' ' or '')..surround_end
+      if indent ~= '' then
+        for i, line in ipairs(replace) do
+          replace[i] = indent..line
+        end
+      end
+
+      vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, replace)
+      vim.api.nvim_win_set_cursor(0, {line_num, string.len(replace[1])})
+    end,
+  },
+})
+
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' },
+    { name = 'cmdline' }
+  })
+})
+
+-- nvim-lspconfig
+local nvim_lsp = require('lspconfig')
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>tp', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>dl', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap('n', '<space>ff', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+end
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'pyright', 'tsserver' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+end
 
 -- telescope
 require('telescope').setup{
