695fecf984e38041872fe5d0f0704904ea2bd0bb
Hillary Chan <hillary.chan91@gmail.com>
2021-12-18 16:49:21 +1100

    Configure prettier-eslint source for null-ls

--- .config/nvim/lua/plugins/nvim-lspconfig/null-ls.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig/null-ls.lua
@@ -1,5 +1,19 @@
 -- TODO: update null_ls config for v0.6
 local null_ls = require('null-ls')
+local helpers = require('null-ls.helpers')
+local methods = require('null-ls.methods')
+
+local prettier_eslint = {
+  name = 'prettier-eslint',
+  method = methods.internal.FORMATTING,
+  filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+  generator = helpers.formatter_factory({
+    command = 'prettier-eslint',
+    args = { '$FILENAME' },
+    to_stdin = true,
+  }),
+  factory = helpers.formatter_factory,
+}
 
 null_ls.config({
   sources = {
@@ -9,6 +23,7 @@ null_ls.config({
       filetypes = { 'css', 'scss', 'less', 'html', 'json', 'yaml' },
     }),
     null_ls.builtins.formatting.stylua,
+    prettier_eslint,
 
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.diagnostics.shellcheck.with({ filetypes = { 'sh', 'zsh', 'bash' } }),
