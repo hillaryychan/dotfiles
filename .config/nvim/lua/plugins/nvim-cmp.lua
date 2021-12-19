5685af19b0cffe1b0f4247f435a092f7a8500f20
Hillary Chan <hillary.chan91@gmail.com>
2021-12-19 13:19:03 +1100

    Fix custom snippet expander to deal with trailing chars

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -47,17 +47,20 @@ cmp.setup({
   }),
   snippet = {
     -- Don't want to use actual snippet engine.
-    -- This snippet expander just inputs selected text and places cursor at the end.
+    -- This snippet expander just inputs selected text and
+    -- places cursor at the end of first inputted text
     expand = function(args)
       local line_num, col = unpack(vim.api.nvim_win_get_cursor(0))
       local line_text = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, true)[1]
       local indent = string.match(line_text, '^%s*')
       local replace = vim.split(args.body, '\n', true)
+      local replace_col = col - #indent
       local surround = string.match(line_text, '%S.*') or ''
-      local surround_end = surround:sub(col)
+      local surround_end = surround:sub(replace_col + 1)
+      local cursor_col = col + #replace[1]
 
-      replace[1] = surround:sub(0, col - 1) .. replace[1]
-      replace[#replace] = replace[#replace] .. (#surround_end > 1 and ' ' or '') .. surround_end
+      replace[1] = surround:sub(0, replace_col) .. replace[1]
+      replace[#replace] = replace[#replace] .. surround_end
       if indent ~= '' then
         for i, line in ipairs(replace) do
           replace[i] = indent .. line
@@ -65,7 +68,7 @@ cmp.setup({
       end
 
       vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, replace)
-      vim.api.nvim_win_set_cursor(0, { line_num, string.len(replace[1]) })
+      vim.api.nvim_win_set_cursor(0, { line_num, cursor_col })
     end,
   },
 })
