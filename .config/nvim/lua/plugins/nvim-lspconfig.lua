27ed048f5e82f14710cd7911a9091d144d8616b4
Hillary Chan <hillary.chan91@gmail.com>
2022-10-17 20:03:59 +1100

    nvim: use default_capabilities

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,6 @@
 local nvim_lsp = require('lspconfig')
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local function on_attach(client, bufnr)
   require('utils').on_attach_base(client, bufnr)
