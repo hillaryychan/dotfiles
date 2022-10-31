93938a42aab6ad358e31e9aa4d0c43235941fa0c
Hillary Chan <hillary.chan91@gmail.com>
2022-10-31 22:31:37 +1100

    nvim: commands for copying file paths

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -36,3 +36,26 @@ vim.api.nvim_set_keymap('n', '<C-j>', '<C-w><C-j>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w><C-k>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w><C-l>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-h>', '<C-w><C-h>', { noremap = true })
+
+-- copy path names
+function _G.copy_path_name(path_type)
+  return function()
+    -- default to relative path
+    local path = vim.fn.expand('%')
+    if path_type == 'full' then
+      path = vim.fn.expand('%:p')
+    elseif path_type == 'filename' then
+      path = vim.fn.expand('%:t')
+    end
+
+    if path == '' then
+      print('No file name to copy')
+    else
+      vim.fn.setreg('+', path)
+      print(string.format('Copied "%s"', path))
+    end
+  end
+end
+vim.api.nvim_create_user_command('CopyRelativePath', copy_path_name('relative'), {})
+vim.api.nvim_create_user_command('CopyFilePath', copy_path_name('full'), {})
+vim.api.nvim_create_user_command('CopyFileName', copy_path_name('filename'), {})
