1239a9aa7501841fe68b1b94a122e6e8031239e5
Hillary Chan <hillary.chan91@gmail.com>
2024-03-12 23:48:15 +1100

    nvim: use nvim-snippy for snippet engine

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -49,8 +49,8 @@ require('lazy').setup({
       'hrsh7th/cmp-cmdline',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
+      'dcampos/nvim-snippy',
+      'dcampos/cmp-snippy',
     },
     config = function()
       require('plugins.nvim-cmp')
--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,5 +1,4 @@
 local cmp = require('cmp')
-local luasnip = require('luasnip')
 
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -9,8 +8,6 @@ end
 local select_prev_item = function(fallback)
   if cmp.visible() then
     cmp.select_next_item()
-  elseif luasnip.expand_or_jumpable() then
-    luasnip.expand_or_jump()
   elseif has_words_before() then
     cmp.complete()
   else
@@ -21,8 +18,6 @@ end
 local select_next_item = function(fallback)
   if cmp.visible() then
     cmp.select_prev_item()
-  elseif luasnip.jumpable(-1) then
-    luasnip.jump(-1)
   else
     fallback()
   end
@@ -53,11 +48,11 @@ cmp.setup({
     { name = 'nvim_lsp' },
     { name = 'buffer' },
     { name = 'path' },
-    { name = 'luasnip' },
+    { name = 'snippy' },
   }),
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      require('snippy').expand_snippet(args.body)
     end,
   },
 })
