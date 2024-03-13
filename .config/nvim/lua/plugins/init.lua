d154e82361ee9a2643fd00fb1eabc0c64fd06a63
Hillary Chan <hillary.chan91@gmail.com>
2024-03-13 18:58:48 +1100

    nvim: fix treesitter dependencies

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -27,17 +27,17 @@ require('lazy').setup({
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    dependencies = {
+      {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+        config = function()
+          vim.g.skip_ts_context_commentstring_module = true
+          require('ts_context_commentstring').setup()
+        end,
+      },
+    },
     config = function()
       require('plugins.nvim-treesitter')
-    end,
-  },
-  {
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    config = function()
-      vim.g.skip_ts_context_commentstring_module = true
-      require('ts_context_commentstring').setup({
-        enable_autocmd = false,
-      })
     end,
   },
 
