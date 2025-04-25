c831127f5063ed0541d883c0ed24fe72cccf103a
Hillary Chan <hillary.chan91@gmail.com>
2025-04-25 15:35:12 +1000

    nvim: remove null-ls config and on_attach utils

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -4,32 +4,4 @@ function M.t(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-function M.on_attach_base(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', '<A-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<leader>tp', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<A-d>', vim.diagnostic.open_float, bufopts)
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
-  vim.keymap.set('n', '<leader>=', function()
-    vim.lsp.buf.format({ async = true })
-  end, bufopts)
-
-  vim.api.nvim_create_user_command('WorkspaceFolders', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, {})
-end
-
 return M
